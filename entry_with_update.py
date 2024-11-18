08ff83cda3cea9dc30e8be131c2d5f890ae48222
metercai <cai.mingjun@gmail.com>
2024-11-18 22:57:06 +0800

    change --dev to i2i branche

--- entry_with_update.py
+++ entry_with_update.py
@@ -22,7 +22,7 @@ try:
 
     origin_name = 'main'
     main_name = 'SimpleSDXL'
-    dev_name = 'SimpleSDXL_dev'
+    dev_name = 'identity_flux.i2i'
     local_branch_ref = f'refs/heads/{branch_name}'
     if '--dev' in (sys.argv):
         if branch_name != dev_name:
