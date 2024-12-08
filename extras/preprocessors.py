9dfbbe60a17d20db1d413e629ee29cf772a9d348
metercai <cai.mingjun@gmail.com>
2024-12-09 06:36:48 +0800

    fixed

--- extras/preprocessors.py
+++ extras/preprocessors.py
@@ -80,7 +80,7 @@ def cpds(x):
 
     return norm255(result, low=4, high=96).clip(0, 255).astype(np.uint8)
 
-from extras.easy_dwpose import DWposeDetector
+from extras.easy_dwpose.dwpose import DWposeDetector
 def dwpose(x):
     detector = DWposeDetector()
     result = detector(x, output_type="np", include_hands=False, include_face=False)
